a7f13204744c8d5e91c53291ecb0dfa24f5c4b1e
Nafis Rayan <nafisrayan123@gmail.com>
2025-04-19 20:51:07 +0600

    Add Bison parser header file with token definitions and parser interface
    - Created a new header file `y.tab.h` for the Bison parser.
    - Defined token types for various language constructs including control flow, data types, operators, and punctuation.
    - Included necessary preprocessor directives for parser configuration and debugging.
    - Established the `YYSTYPE` type for semantic values associated with tokens.

--- script.sh
+++ script.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Clean up any previous outputs
+rm -f lex.yy.c y.tab.c y.tab.h y.output *.o a.exe 21301559_log.txt
+
+# Generate the parser (yacc)
 yacc -d -y --debug --verbose syntax_analyzer.y
 echo 'Generated the parser C file as well the header file'
+
+# Generate parser object file
 g++ -w -c -o y.o y.tab.c
 echo 'Generated the parser object file'
+
+# Generate the scanner (flex)
 flex lex_analyzer.l
 echo 'Generated the scanner C file'
-g++ -fpermissive -w -c -o l.o lex.yy.c
-# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
+
+# Generate scanner object file
+g++ -std=c++17 -fpermissive -w -c -o l.o lex.yy.c
 echo 'Generated the scanner object file'
-g++ y.o l.o
+
+# Link and create executable
+g++ -std=c++17 y.o l.o
 echo 'All ready, running'
-./a.exe input.c
-echo 'logfile'
-cat log.txt
+
+# Run test cases
+for input in InputOutput/input*.c; do
+    if [ -f "$input" ]; then
+        echo "Processing $input..."
+        ./a.exe "$input"
+        echo "Done. Log file generated as 21301559_log.txt"
+        echo "----------------------------------------"
+    fi
+done
--- symbol_info.h
+++ symbol_info.h
@@ -6,38 +6,95 @@ class symbol_info
 private:
     string name;
     string type;
-
-    // Write necessary attributes to store what type of symbol it is (variable/array/function)
-    // Write necessary attributes to store the type/return type of the symbol (int/float/void/...)
-    // Write necessary attributes to store the parameters of a function
-    // Write necessary attributes to store the array size if the symbol is an array
+    string kind;         // "variable", "array", or "function"
+    string data_type;    // "int", "float", "void", etc.
+    int array_size;      // -1 for non-arrays
+    vector<string> param_types;  // parameter types for functions
+    symbol_info* next;   // for hash table chaining
 
 public:
     symbol_info(string name, string type)
     {
         this->name = name;
         this->type = type;
+        this->kind = "variable";  // default
+        this->data_type = "";
+        this->array_size = -1;
+        this->next = nullptr;
     }
-    string get_name()
-    {
-        return name;
-    }
-    string get_type()
-    {
-        return type;
-    }
-    void set_name(string name)
+
+    // Constructor for variables
+    symbol_info(string name, string type, string data_type)
     {
         this->name = name;
-    }
-    void set_type(string type)
-    {
         this->type = type;
+        this->kind = "variable";
+        this->data_type = data_type;
+        this->array_size = -1;
+        this->next = nullptr;
     }
-    // Write necessary functions to set and get the attributes
+
+    // Constructor for arrays
+    symbol_info(string name, string type, string data_type, int size)
+    {
+        this->name = name;
+        this->type = type;
+        this->kind = "array";
+        this->data_type = data_type;
+        this->array_size = size;
+        this->next = nullptr;
+    }
+
+    // Constructor for functions
+    symbol_info(string name, string type, string return_type, vector<string> params)
+    {
+        this->name = name;
+        this->type = type;
+        this->kind = "function";
+        this->data_type = return_type;  // stores return type for functions
+        this->array_size = -1;
+        this->param_types = params;
+        this->next = nullptr;
+    }
+
+    // Getters
+    string get_name() { return name; }
+    string get_type() { return type; }
+    string get_kind() { return kind; }
+    string get_data_type() { return data_type; }
+    int get_array_size() { return array_size; }
+    vector<string> get_param_types() { return param_types; }
+    symbol_info* get_next() { return next; }
+
+    // Setters
+    void set_name(string name) { this->name = name; }
+    void set_type(string type) { this->type = type; }
+    void set_kind(string kind) { this->kind = kind; }
+    void set_data_type(string data_type) { this->data_type = data_type; }
+    void set_array_size(int size) { this->array_size = size; }
+    void set_param_types(vector<string> params) { this->param_types = params; }
+    void set_next(symbol_info* next) { this->next = next; }
+
+    string to_string() const {
+        string info = "<" + name + ", " + type;
+        if (kind == "array") {
+            info += ", " + data_type + "[" + std::to_string(array_size) + "]";
+        }
+        else if (kind == "function") {
+            info += ", " + data_type + ", {";
+            for (size_t i = 0; i < param_types.size(); i++) {
+                if (i > 0) info += ", ";
+                info += param_types[i];
+            }
+            info += "}";
+        }
+        info += ">";
+        return info;
+    }
 
     ~symbol_info()
     {
-        // Write necessary code to deallocate memory, if necessary
+        // No dynamic memory allocated in this class
+        // next pointer will be managed by scope_table
     }
 };
